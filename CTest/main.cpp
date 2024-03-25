209ed54f68ae41c635a8bea2d6a33ee4945aa253
n-takei <163802887+n-takei@users.noreply.github.com>
2024-03-25 10:30:29 +0900

    コード大幅変更

--- CTest/main.cpp
+++ CTest/main.cpp
@@ -9,6 +9,12 @@
 #include<ctime>
 #include<mutex>
 #include<thread>
+#include <queue>
+#include <filesystem>
+
+#define MAX_ROW 65536
+#define NUMBER_OF_THREAD 5
+
 class my {
 private:
 	std::string name;
@@ -62,6 +68,12 @@ void safe_print(my x)
     x.Show();
 }
 
+void safe_print(std::string x)
+{
+    std::lock_guard<std::mutex> lock(print_mtx_);
+    std::cout << x << std::endl;
+}
+
 class ProfileList {
 private:
     std::vector<my> list;
@@ -90,61 +102,211 @@ public:
     }
 };
 
+class ThreadTest
+{
+private:
+    std::vector<std::string> list;
+    std::mutex mtx_;
+public:
+    std::unique_lock<std::mutex> get_lock()
+    {
+        return std::unique_lock<std::mutex>(mtx_); // ロックを取得する
+    }
+    // vectorオブジェクトへのアクセスを排他的にする
+    void add_value(std::string value)
+    {
+        std::unique_lock<std::mutex> lk = get_lock(); // ロックされたunique_lockを受け取る
+
+        list.push_back(value);
+
+    } // ロックを手放す(unique_lockのデストラクタ)
+
+    void print()
+    {
+        std::unique_lock<std::mutex> lk = get_lock();
+
+        for (std::string x : list) {
+            safe_print(x);
+        }
+    }
+
+    int number_of_list() {
+        return list.size();
+    }
+};
+
+std::mutex que_mtx_;
+
+/**
+* @brief フォルダ以下のファイル一覧を取得する関数
+* @param[in]    folderPath  フォルダパス
+* @param[out]   file_names  ファイル名一覧
+* return        true:成功, false:失敗
+*/
+bool getFileNames(std::string folderPath, std::vector<std::string>& file_names)
+{
+    using namespace std::filesystem;
+    directory_iterator iter(folderPath), end;
+    std::error_code err;
+
+    for (; iter != end && !err; iter.increment(err)) {
+        const directory_entry entry = *iter;
+
+        file_names.push_back(entry.path().string());
+        //printf("%s\n", file_names.back().c_str());
+    }
+
+    /* エラー処理 */
+    if (err) {
+        std::cout << err.value() << std::endl;
+        std::cout << err.message() << std::endl;
+        return false;
+    }
+    return true;
+}
+
 int main()
 {
-    ProfileList list;
-    std::vector<my> stack;
+    std::vector<std::pair<std::string, std::vector<std::string>>> q;
+    std::vector<std::thread> thds;
+    const std::string monitorDir = "C:\\temp2";
+    const std::string moveToDir = "C:\\moveTo";
+    std::string endFile = "end";
+    bool processEnd = false;
 
-    std::thread t1([&list] {
-        std::tm birthDate = { 0,0,0,15,3,1995 };
-        my data1 = my("安田", birthDate);
-        list.add_value(data1);
-    });
+    // id=1..5のスレッドを作成
+    for (int id = 1; id <= NUMBER_OF_THREAD; id++) {
+        // ループ変数idは必ずコピーキャプチャする
+        //thds.emplace_back([&thTest, &q, &genVal, id] {
+        thds.emplace_back([&q, &processEnd, id] {
 
-    std::thread t2([&list] {
-        std::tm birthDate = { 0,0,0,1,10,1996 };
-        my data1 = my("佐藤", birthDate);
-        list.add_value(data1);
-    });
+            std::string searchWord = "#region";
+            const std::string outLogDir = "C:\\outputLog";
+            std::string filePath;
+            std::vector<std::string> v;
+            // 新しいスレッドで実行される処理
+            while (!processEnd)
+            {
+                std::string logFilePath;
 
-    
-    t1.join();
-    t2.join();
+                std::this_thread::sleep_for(std::chrono::milliseconds(50));
+                {
+                    std::unique_lock<std::mutex> uniq_lk(que_mtx_);// ここでロックされる
+                
+                    if (!q.empty()) {
+                        filePath = q.back().first;
+                        v = q.back().second;
+                        //thTest.add_value(v);
+                        q.pop_back();
+                        safe_print(std::format("【スレッド{0}】値[ {1} ]がキューから取り出されました。(現在のキュー個数={2})", id, filePath, q.size()));
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
 
-    list.print();
-	//std::vector<my> list = std::vector<my>();
+                const std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+                std::time_t end_time = std::chrono::system_clock::to_time_t(now);
+                tm* nowDate = std::gmtime(&end_time);
+                const std::string nowDateStr;
+                std::stringstream ss;
+                ss << std::setw(4) << std::setfill('0') << nowDate->tm_year + 1900 <<
+                    std::setw(2) << std::setfill('0') << nowDate->tm_mon + 1 <<
+                    std::setw(2) << std::setfill('0') << nowDate->tm_mday <<
+                    std::setw(2) << std::setfill('0') << nowDate->tm_hour <<
+                    std::setw(2) << std::setfill('0') << nowDate->tm_min <<
+                    std::setw(2) << std::setfill('0') << nowDate->tm_sec;
 
-    //std::tm birthDate = { 0,0,0,15,3,1995 };
-    //list.push_back(my("武井　直哉", birthDate));
+                //thTest.add_value(v);
+                std::ofstream iof(outLogDir + "\\" + filePath.substr(filePath.find_last_of("\\") + 1));
 
- //   //my myinfo = list[0];
- //   //myinfo.Show();
+                int foundCnt = 0;
+                int rowNumber = 1;
+                for (std::string buf : v) {
+                    int foundIdx = 0;
+                    int foundCntRow = 0;
+                    do{
+                        foundIdx = buf.find(searchWord, foundIdx);
+                        if (foundIdx != std::string::npos) {
+                            safe_print(std::format("【スレッド{0}】{1}行目{2}列目で文字列[ {3} ]がヒットしました。", id, rowNumber, foundIdx + 1, searchWord));
+                            foundCntRow++;
+                            foundIdx += searchWord.length();
+                        }
+                    } while (foundIdx != std::string::npos);
+                    foundCnt += foundCntRow;
+                    rowNumber++;
+                }
+                    
+                if (foundCnt > 0) {
+                    safe_print(std::format("【スレッド{0}】文字列[ {1} ]は全部で{2}個見つかりました。", id, searchWord, foundCnt));
+                }
+                else {
+                    safe_print(std::format("【スレッド{0}】文字列[ {1} ]は見つかりませんでした。", id, searchWord));
+                }
+            }
+        });
+    }
 
- //   std::ifstream ifs("C:\\Users\\n.takei\\Desktop\\MUMSS\\support_status_20240313_1.csv");
- //   std::string buf;
+    while (!processEnd)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
- //   if (ifs.fail()) {
- //       std::cout << "ファイルオープンエラー" << std::endl;
- //       return -1;
- //   }
+        std::vector<std::string> filePathes;
+        getFileNames(monitorDir, filePathes);
 
- //   while (std::getline(ifs, buf)) {
- //       std::cout << buf << std::endl;
- //   }
-	//const std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
- //   std::time_t end_time = std::chrono::system_clock::to_time_t(now);
- //   std::cout << "Current Time and Date: " << std::ctime(&end_time) << std::endl;
+        for (std::string filePath : filePathes) {
 
-	//std::cout << std::endl;
-	//std::cout << "from Unix Time Stamp millisecond" << std::endl;
-	//const std::chrono::system_clock::duration duration = now.time_since_epoch();
-	//const long long ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-	//std::cout << "       ms: " << ms << std::endl;
+            std::string fileName = filePath.substr(filePath.find_last_of("\\") + 1);
+            if (endFile == fileName) {
+                processEnd = true;
+                std::filesystem::remove(filePath);
+                break;
+            }
 
- //   const long long sec = ms / 1000;
- //   std::tm* gt = std::gmtime(&sec);
- //   output("   gmtime: ", gt, ms % 1000);
+            bool exist = false;
+            //for (std::string d : q) {
+            for (int i = 0; i < q.size(); i++) {
+                //if (fileName == d) {
+                if(filePath == q[i].first){
+                    exist = true;
+                    break;
+                }
+            }
+            if (exist) {
+                continue;
+            }
 
- //   std::tm* lt = std::localtime(&sec);
- //   output("localtime: ", lt, ms % 1000);
+
+            std::ifstream ifs(filePath);
+
+            if (ifs.fail()) {
+               safe_print(std::format("ファイルオープンエラー [ {0} ]", filePath));
+               continue;
+            }
+            else {
+                safe_print(std::format("ファイルオープン [ {0} ]", filePath));
+                {
+                    std::unique_lock<std::mutex> uniq_lk(que_mtx_); // ここでロックされる
+                    q.push_back(std::pair<std::string, std::vector<std::string>>(filePath, std::vector<std::string>()));
+
+                    std::string buf;
+                    while (std::getline(ifs, buf)) {
+                        //q.push_back(buf);
+                        q.back().second.push_back(buf);
+                    }
+                }
+                safe_print(std::format("値[ {0} ]がキューに追加されました。(現在のキュー個数={1})", filePath, q.size()));
+
+                ifs.close();
+                //std::filesystem::rename(filePath, std::format("{0}\\{1}", moveToDir, fileName));
+                std::filesystem::remove(filePath);
+            }
+        }
+    }
+
+    // 全てのスレッド処理完了を待機
+    for (auto& t : thds) {
+        t.join();
+    }
 }
